4c973a098c14fd5393639ac39fc4bfdaf279026c
HeyyD <hmhati@gmail.com>
2020-03-31 18:29:43 +0300

    Create new state

--- core/src/fi/chaocompany/online/Main.java
+++ core/src/fi/chaocompany/online/Main.java
@@ -1,33 +1,19 @@
 package fi.chaocompany.online;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Main extends Game {
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new RoomState());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
