ee4bc84a7b599fa91fd270e6e7e820dcf2b190cd
HeyyD <hmhati@gmail.com>
2020-04-01 19:12:29 +0300

    Add camera

--- core/src/fi/chaocompany/online/RoomState.java
+++ core/src/fi/chaocompany/online/RoomState.java
@@ -2,17 +2,25 @@ package fi.chaocompany.online;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 import fi.chaocompany.online.map.TileMap;
 
-public class RoomState implements Screen {
+public class RoomState implements Screen, GestureDetector.GestureListener {
 
     private SpriteBatch batch;
     private TileMap tileMap;
+    private OrthographicCamera camera;
 
     public RoomState() {
+
+        Gdx.input.setInputProcessor(new GestureDetector(this));
+
         batch = new SpriteBatch();
         int[][] map = new int[][]{
                 {0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8},
@@ -31,6 +39,9 @@ public class RoomState implements Screen {
 
         Texture texture = new Texture("tileset.png");
         this.tileMap = new TileMap(map, texture);
+
+        this.camera = new OrthographicCamera();
+        this.camera.setToOrtho(false);
     }
 
     @Override
@@ -42,6 +53,7 @@ public class RoomState implements Screen {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.setProjectionMatrix(this.camera.combined);
         batch.begin();
         this.tileMap.drawMap(batch);
         batch.end();
@@ -69,5 +81,50 @@ public class RoomState implements Screen {
     @Override
     public void dispose() {
         batch.dispose();
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
     }
 }
