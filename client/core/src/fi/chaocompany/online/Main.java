a3b0aaf2f00b3318dff27d0a28aacf432a63a667
HeyyD <hmhati@gmail.com>
2020-04-08 15:56:16 +0300

    Create websocket for client and try to connect

--- client/core/src/fi/chaocompany/online/Main.java
+++ client/core/src/fi/chaocompany/online/Main.java
@@ -1,12 +1,23 @@
 package fi.chaocompany.online;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import fi.chaocompany.online.network.WebSocket;
 import fi.chaocompany.online.state.RoomState;
 
 public class Main extends Game {
 	@Override
 	public void create () {
 		setScreen(new RoomState());
+
+		new Thread(() -> {
+			try {
+				WebSocket.getInstance();
+			} catch (Exception e) {
+				Gdx.app.log("MAIN", "FAILED TO CONNECT");
+				e.printStackTrace();
+			}
+		}).start();
 	}
 
 	@Override
---
+++ client/core/src/fi/chaocompany/online/network/WebSocket.java
@@ -0,0 +1,67 @@
+package fi.chaocompany.online.network;
+
+import com.badlogic.gdx.Gdx;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.lang.reflect.Type;
+import java.util.Scanner;
+
+public class WebSocket {
+
+    private final static String LOG_TAG = WebSocket.class.getSimpleName();
+
+    private static WebSocket instance;
+
+    private WebSocket() {
+        WebSocketClient client = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSessionHandler sessionHandler = new StompSessionHandler() {
+            @Override
+            public void afterConnected(StompSession stompSession, StompHeaders stompHeaders) {
+                Gdx.app.log(LOG_TAG, "CONNECTED");
+                // stompSession.subscribe("/topic/messages", this);
+                // stompSession.send("/app/chat", "Hello world!");
+            }
+
+            @Override
+            public void handleException(StompSession stompSession, StompCommand stompCommand, StompHeaders stompHeaders, byte[] bytes, Throwable throwable) {
+
+            }
+
+            @Override
+            public void handleTransportError(StompSession stompSession, Throwable throwable) {
+
+            }
+
+            @Override
+            public Type getPayloadType(StompHeaders stompHeaders) {
+                return null;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders stompHeaders, Object o) {
+                // String msg = (String) o;
+                // Gdx.app.log(LOG_TAG, "Received : " + msg);
+            }
+        };
+
+        stompClient.connect("ws://localhost:8080", sessionHandler);
+        new Scanner(System.in).nextLine();
+    }
+
+    public static WebSocket getInstance() {
+        if (instance == null) {
+            instance = new WebSocket();
+        }
+        return instance;
+    }
+}
