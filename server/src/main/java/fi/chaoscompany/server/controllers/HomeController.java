8c160931daa80581db2341ed82cf8cd84be160bf
HeyyD <hmhati@gmail.com>
2020-04-08 16:35:49 +0300

    Create hello world in spring

---
+++ server/src/main/java/fi/chaoscompany/server/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package fi.chaoscompany.server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HomeController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
