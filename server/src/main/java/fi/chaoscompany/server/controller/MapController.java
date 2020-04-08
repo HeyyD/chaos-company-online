23a88ab976e5c8bc69808658ac15cecc060e523e
HeyyD <hmhati@gmail.com>
2020-04-08 23:02:50 +0300

    Make map controller to use REST

--- server/src/main/java/fi/chaoscompany/server/controller/MapController.java
+++ server/src/main/java/fi/chaoscompany/server/controller/MapController.java
@@ -3,12 +3,15 @@ package fi.chaoscompany.server.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/map")
 public class MapController {
+    Logger logger = LoggerFactory.getLogger(MapController.class);
+
     private final int[][] map = new int[][]{
             {0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8},
             {1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 7},
@@ -24,12 +27,8 @@ public class MapController {
             {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 0}
     };
 
-    Logger logger = LoggerFactory.getLogger(MapController.class);
-
-    @MessageMapping("/map")
-    @SendTo("/map")
-    public MapMessage send(MapMessage msg) {
-        logger.info("Received tile map request");
-        return new MapMessage(map);
+    @RequestMapping(method = RequestMethod.GET)
+    public String saveBlogPost() {
+        return "Hello World!";
     }
 }
