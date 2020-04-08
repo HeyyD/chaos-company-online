9db1fa33ec266a07d1b51929138b50610e02bc53
HeyyD <hmhati@gmail.com>
2020-04-08 11:05:26 +0300

    Delete old stuff

--- core/src/fi/chaocompany/online/map/TileConstants.java
+++
@@ -1,8 +0,0 @@
-package fi.chaocompany.online.map;
-
-public class TileConstants {
-    public static final int TILE_WIDTH_PIXELS = 128;
-    public static final int TILE_HEIGHT_PIXELS = 64;
-    public static final int TILESET_WIDTH = 2;
-    public static final int TILESET_HEIGHT = 5;
-}
--- core/src/fi/chaocompany/online/player/PlayerConstants.java
+++
@@ -1,8 +0,0 @@
-package fi.chaocompany.online.player;
-
-public class PlayerConstants {
-    public static int SPRITE_SHEET_WIDTH = 4;
-    public static int SPRITE_SHEET_HEIGHT = 4;
-    public static int FRAME_WIDTH_PIXELS = 128;
-    public static int FRAME_HEIGHT_PIXELS = 128;
-}
