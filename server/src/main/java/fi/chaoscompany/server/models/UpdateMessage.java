0b07a0e804ad9f039a71b907e7710efa786dc49b
HeyyD <hmhati@gmail.com>
2020-04-10 11:42:12 +0300

    Update message base

--- server/src/main/java/fi/chaoscompany/server/models/UpdateMessage.java
+++ server/src/main/java/fi/chaoscompany/server/models/UpdateMessage.java
@@ -1,19 +1,37 @@
 package fi.chaoscompany.server.models;
 
 public class UpdateMessage {
-    String msg;
+    private int id;
+    private float x;
+    private float y;
 
-    public UpdateMessage() {}
-
-    public UpdateMessage(String msg) {
-        this.msg = msg;
+    public UpdateMessage(int id, float x, float y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
     }
 
-    public String getMsg() {
-        return msg;
+    public int getId() {
+        return id;
     }
 
-    public void setMsg(String msg) {
-        this.msg = msg;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
     }
 }
