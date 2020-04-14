de0428f39d986926b5e071063e2362927baaf3f0
HeyyD <hmhati@gmail.com>
2020-04-14 13:27:00 +0300

    Make window unresizable

--- client/desktop/src/fi/chaocompany/online/desktop/DesktopLauncher.java
+++ client/desktop/src/fi/chaocompany/online/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.forceExit = false;
+		config.resizable = false;
 		new LwjglApplication(new Main(), config);
 	}
 }
