c07449492e1ee24849231ecab082d6d67c3c9cc7
HeyyD <hmhati@gmail.com>
2020-04-19 11:27:19 +0300

    Add linting to project

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,17 +1,18 @@
-import "phaser";
+import * as Phaser from 'phaser';
 
 const config = {
-  title: "Starfall",
+  title: 'Starfall',
   width: 800,
   height: 600,
-  parent: "game",
-  backgroundColor: "#18216D"
+  parent: 'game',
+  backgroundColor: '#18216D',
 };
 export class StarfallGame extends Phaser.Game {
   constructor(config) {
     super(config);
   }
 }
-window.onload = () => {
-  var game = new StarfallGame(config);
+
+window.onload = (): void => {
+  new StarfallGame(config);
 };
