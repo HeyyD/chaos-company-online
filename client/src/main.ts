f4db83e250b6ff4f8b7ed0a6d3feb7de4e96fbd6
HeyyD <hmhati@gmail.com>
2020-04-20 21:04:45 +0300

    Remove everything that is not needed

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,60 +2,23 @@ import * as Phaser from 'phaser';
 
 function onPreload(): void {
   console.log('on preload');
-  this.load.setBaseURL('http://labs.phaser.io');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
 }
 
 function onCreate(): void {
   console.log('on create');
-
-  this.add.image(400, 300, 'sky');
-
-  const particles = this.add.particles('red');
-
-  const emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0, },
-    blendMode: 'ADD',
-  });
-
-  const logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
 }
 
 const config: Phaser.Types.Core.GameConfig = {
-  title: 'Starfall',
+  title: 'Chaos Company',
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  parent: 'game',
-  backgroundColor: '#18216D',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200, },
-    },
-  },
   scene: {
     preload: onPreload,
     create: onCreate,
   },
 };
 
-export class StarfallGame extends Phaser.Game {
-  constructor(config: Phaser.Types.Core.GameConfig) {
-    super(config);
-  }
-}
-
 window.onload = (): void => {
-  new StarfallGame(config);
+  new Phaser.Game(config);
 };
