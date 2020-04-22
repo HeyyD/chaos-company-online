a8e6654415979b539f7e59d2ff387c04b8bddd6a
HeyyD <hmhati@gmail.com>
2020-04-22 17:05:50 +0300

    Pan camera with mouse wheel

--- client/src/camera/gestures.ts
+++ client/src/camera/gestures.ts
@@ -1,19 +1,22 @@
 export const pan = (scene: Phaser.Scene, camera: Phaser.Cameras.Scene2D.Camera): void => {
+  // pan button is mouse wheel, the integer code is 1
+  if (scene.input.activePointer.button === 1) {
+    scene.input.off('pointerdown');
 
-  const originX = scene.input.activePointer.worldX;
-  const originY = scene.input.activePointer.worldY;
-
-  scene.input.off('pointerdown');
-  scene.input.on('pointermove', () => {
-    const x = originX - scene.input.activePointer.worldX;
-    const y = originY - scene.input.activePointer.worldY;
+    const originX = scene.input.activePointer.worldX;
+    const originY = scene.input.activePointer.worldY;
   
-    camera.setScroll(camera.scrollX + x, camera.scrollY + y);
-  });
-
-  scene.input.on('pointerup', () => {
-    scene.input.off('pointermove');
-    scene.input.off('pointerup');
-    scene.input.on('pointerdown', () => pan(scene, camera));
-  });
+    scene.input.on('pointermove', () => {
+      const x = originX - scene.input.activePointer.worldX;
+      const y = originY - scene.input.activePointer.worldY;
+    
+      camera.setScroll(camera.scrollX + x, camera.scrollY + y);
+    });
+  
+    scene.input.on('pointerup', () => {
+      scene.input.off('pointermove');
+      scene.input.off('pointerup');
+      scene.input.on('pointerdown', () => pan(scene, camera));
+    }); 
+  }
 };
